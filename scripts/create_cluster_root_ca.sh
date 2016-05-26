093d83b2333c5e5d55b16da9c285e86301883d0c
Sam Lau <esayemm@gmail.com>
2016-05-26 16:29:02 -0700

    use cfssl to generate certs

--- scripts/create_cluster_root_ca.sh
+++ scripts/create_cluster_root_ca.sh
@@ -3,11 +3,13 @@ cd "$parent_path"
 
 rm -rf ../certs
 mkdir -p ../certs
+cd ../certs
 
 echo "creating ca-key.pem and ca.pem in certs/ ..."
-openssl genrsa -out ../certs/ca-key.pem 2048
-openssl req -x509 -new -nodes \
-  -key ../certs/ca-key.pem \
-  -days 10000 \
-  -out ../certs/ca.pem \
-  -subj "/CN=kube-ca"
+cfssl gencert -initca ../coreos_certs/ca-csr.json | cfssljson -bare ca -
+# openssl genrsa -out ../certs/ca-key.pem 2048
+# openssl req -x509 -new -nodes \
+#   -key ../certs/ca-key.pem \
+#   -days 10000 \
+#   -out ../certs/ca.pem \
+#   -subj "/CN=kube-ca"
